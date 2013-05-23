32f2cd48d04e4053bf40259aff071ef934ec34fa
Justin Lin <Justin1021@gmail.com>
2013-05-23 17:34:48 +0800

    Test

---
+++ ProgLabUtil/EncryptUtil.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Security.Cryptography;
+
+namespace ProgLab.Util
+{
+    public class EncryptUtil
+    {
+        /// <summary>
+        /// 加密
+        /// </summary>
+        /// <param name="_strQ"></param>
+        /// <param name="strKey">要8個字元</param> 
+        /// <param name="strIV">要>=8個字元</param>
+        /// <returns></returns>
+        public static string Encrypt(string _strQ, string strKey, string strIV)
+        {
+            byte[] buffer = Encoding.UTF8.GetBytes(_strQ);
+            MemoryStream ms = new MemoryStream();
+            DESCryptoServiceProvider tdes = new DESCryptoServiceProvider();
+            CryptoStream encStream = new CryptoStream(ms, tdes.CreateEncryptor(Encoding.UTF8.GetBytes(strKey), Encoding.UTF8.GetBytes(strIV)), CryptoStreamMode.Write);
+            encStream.Write(buffer, 0, buffer.Length);
+            encStream.FlushFinalBlock();
+            return Convert.ToBase64String(ms.ToArray()).Replace("+", "%");
+        }
+        /// <summary>
+        /// 解密
+        /// </summary>
+        /// <param name="_strQ"></param>
+        /// <param name="strKey">要8個字元</param> 
+        /// <param name="strIV">要>=8個字元</param>
+        /// <returns></returns>
+        public static string Decrypt(string _strQ, string strKey, string strIV)
+        {
+            _strQ = _strQ.Replace("%", "+");
+            byte[] buffer = Convert.FromBase64String(_strQ);
+            MemoryStream ms = new MemoryStream();
+            DESCryptoServiceProvider tdes = new DESCryptoServiceProvider();
+            CryptoStream encStream = new CryptoStream(ms, tdes.CreateDecryptor(Encoding.UTF8.GetBytes(strKey), Encoding.UTF8.GetBytes(strIV)), CryptoStreamMode.Write);
+            encStream.Write(buffer, 0, buffer.Length);
+            encStream.FlushFinalBlock();
+            return Encoding.UTF8.GetString(ms.ToArray());
+        }
+
+        public static void UT()
+        {
+            string src = "Justin1021";
+            string encStr = Encrypt(src, "12345678", "Test45.");
+            string decStr = Decrypt(encStr, "12345678", "Test45.");
+        }
+    }
+}
---
+++ ProgLabUtil/RWDictionary.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace ProgLab.Util
+{
+    public class RWDictionary<TKey, TData>
+    {
+        private ReaderWriterLock lockData = new ReaderWriterLock();
+        private Dictionary<TKey, TData> dicData = new Dictionary<TKey, TData>();
+
+        public TData Query(TKey key, int timeoutPeriod)
+        {
+            try
+            {
+                lockData.AcquireReaderLock(timeoutPeriod);
+                TData result = default(TData);
+                dicData.TryGetValue(key, out result);
+                lockData.ReleaseReaderLock();
+
+                return result;
+            }
+            catch (Exception exp)
+            {
+                return default(TData);
+            }
+        }
+
+        public void Add(TKey key, TData data)
+        {
+            try
+            {
+                lockData.AcquireWriterLock(-1);
+                if (dicData.ContainsKey(key) == false)
+                    dicData.Add(key, data);
+                lockData.ReleaseWriterLock();
+            }
+            catch (Exception exp)
+            {
+                // Timeout
+            }
+        }
+
+        public void Remove(TKey key)
+        {
+            try
+            {
+                lockData.AcquireWriterLock(-1);
+                if (dicData.ContainsKey(key))
+                    dicData.Remove(key);
+                lockData.ReleaseWriterLock();
+            }
+            catch (Exception exp)
+            {
+                // Timeout
+            }
+        }
+
+        public void Clear()
+        {
+            try
+            {
+                lockData.AcquireWriterLock(-1);
+                dicData.Clear();
+                lockData.ReleaseWriterLock();
+            }
+            catch (Exception exp)
+            {
+                // Timeout
+            }
+        }
+    }
+}
